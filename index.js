a3405e4c882b14982b7085c4af92ef036e0dd90d
tonyradogna <tonyradogna123@gmail.com>
2026-04-04 01:16:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -202,6 +202,4 @@ app.get("/", (req, res) => res.send("CampusFit backend running!"));
  
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+ 
